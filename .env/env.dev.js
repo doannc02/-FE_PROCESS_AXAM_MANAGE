b9aff020e510c3bc8c0a27ff5c21307934a7fc8b
doannc02 <sillver47108@gmail.com>
2024-10-16 21:33:58 +0700

    ngrok

--- .env/env.dev.js
+++ .env/env.dev.js
@@ -2,7 +2,7 @@ module.exports = {
   serverRuntimeConfig: {},
   publicRuntimeConfig: {
     SUBDOMAIN: '.dev.apusplatform.com',
-    COMMON_URL: 'https://localhost:7115',
- //  COMMON_URL: 'https://a5b5-1-53-37-28.ngrok-free.app',
+   // COMMON_URL: 'https://localhost:7115',
+     COMMON_URL: 'https://cba8-222-252-203-81.ngrok-free.app',
   },
 }
