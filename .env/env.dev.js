da1616aaa5d6b6b3fbe07c08f2e5aa57ada16a3c
doannc02 <sillver47108@gmail.com>
2024-11-02 20:04:43 +0700

    update

--- .env/env.dev.js
+++ .env/env.dev.js
@@ -2,8 +2,8 @@ module.exports = {
   serverRuntimeConfig: {},
   publicRuntimeConfig: {
     SUBDOMAIN: '.dev.apusplatform.com',
-   // COMMON_URL: 'https://itf.viu.edu.vn:880',
+   COMMON_URL: 'https://itf.viu.edu.vn:880',
     //COMMON_URL: 'http://ae62-222-252-99-253.ngrok-free.app',
-   COMMON_URL: 'https://localhost:7115',
+  // COMMON_URL: 'https://localhost:7115',
   },
 }
