5e93dbf2770981582f156dc8eea97233c25bfa6e
doannc02 <sillver47108@gmail.com>
2024-11-10 00:08:14 +0700

    updateupdate

--- src/components/molecules/UploadBox/components/DialogViewFile/index.tsx
+++ src/components/molecules/UploadBox/components/DialogViewFile/index.tsx
@@ -22,7 +22,7 @@ const DialogViewFile = ({
       title={`Chi tiết file ${nameFile}`}
     >
       <div className=''>
-        <CoreViewFile src={`https://itf.viu.edu.vn:880/qldethi/${src}`} />
+        <CoreViewFile src={`https://itf.viu.edu.vn:880/qldethi${src}`} />
       </div>
     </DialogCustom>
   )
