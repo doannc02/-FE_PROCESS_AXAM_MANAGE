207eef67cb679b384641831ef9088127cfb914a2
doannc02 <sillver47108@gmail.com>
2024-12-14 12:03:53 +0700

    test SSR SEO login

--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -6,13 +6,25 @@ import { combineGssp } from '@/lib/next/gssp/combineGssp'
 import { checkLogin } from '@/lib/next/gssp/middleware/checkLogin'
 import { NextPageWithLayout } from '@/lib/next/types'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import Head from 'next/head'
 
 type Props = HttpResponse<null>
 
 const Page: NextPageWithLayout<Props> = () => <Login />
 
 Page.getLayout = NoneLayout
-Page.getMeta = Meta(() => ({ title: 'Login' }))
+Page.getMeta = Meta(() => ({
+  title: 'Login',
+  headNode: (
+    <Head>
+      <title>Đăng nhập hệ thống quản lý đề thi VIU</title>
+      <meta
+        name='description'
+        content='Đăng nhập vào hệ thống quản lý đề thi VIU, nơi giúp bạn tạo và duyệt các đề thi một cách dễ dàng.'
+      />
+    </Head>
+  ),
+}))
 
 export const getServerSideProps = combineGssp<any>(
   checkLogin(),
